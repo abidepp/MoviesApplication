f1702d19bebb282c3c532d8f35a4ded421fcb7d3
Truetech-PC\Truetech <divya.r@enterprisetouch.co.in>
2018-06-05 16:51:21 +0530

    added youtubeAPI files

---
+++ app/src/main/java/com/example/userpc/myapplication/supportclasses/ConfigurationVideoTube.java
@@ -0,0 +1,11 @@
+package com.example.userpc.myapplication.supportclasses;
+
+/**
+ * Created by Truetech on 6/5/2018.
+ */
+
+public class ConfigurationVideoTube {
+
+    public static final String YOUTUBE_API_KEY = "AIzaSyCqOJI_4y4X9fyWz9paRsjgq2DV7G4PflM ";
+
+}
